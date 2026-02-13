d3c590a280a8f7ccb600e8a70dc4570a4caf8755
devanandana21 <tve24el018@cet.ac.in>
2026-02-13 09:38:38 +0530

    Update MultipleCatch.java

--- src/MultipleCatch.java
+++ src/MultipleCatch.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultipleCatchHandling {
+public class MultipleCatch {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
